cd59106272eebfb6ed65302430108bb3f79c3b6c
ozen-hub <info.ozenhub@gmail.com>
2024-03-20 21:01:47 +0530

    4

--- src/app/modules/user/components/user-my-programs/user-my-programs.component.ts
+++ src/app/modules/user/components/user-my-programs/user-my-programs.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-user-my-programs',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './user-my-programs.component.html',
   styleUrl: './user-my-programs.component.scss'
 })
 export class UserMyProgramsComponent {
-
+  date = new Date();
 }
